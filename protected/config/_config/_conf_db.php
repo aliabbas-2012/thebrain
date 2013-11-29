11773f0fdce185704cf5690420004a2f79b9de47
ali <itsgeniusstar@gmail.com>
2013-11-29 16:21:15 +0500

    test

--- protected/config/_config/_conf_db.php
+++ protected/config/_config/_conf_db.php
@@ -7,8 +7,8 @@
 $conf_component_db = array(
     'connectionString' => 'mysql:host=localhost;dbname=thepuzzle_db', //myprojectnet
     'emulatePrepare' => true,
-    'username' => 'root',
-    'password' => '',
+    'username' => 'sfakhar1_yii',
+    'password' => 'abc123AB',
     'charset' => 'utf8',
 );
 ?>
