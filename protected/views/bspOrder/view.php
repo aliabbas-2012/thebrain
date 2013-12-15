1dd745040ee2ba5ba7d30c07e1e70ef51fff67e8
ali <itsgeniusstar@gmail.com>
2013-12-15 12:28:43 +0500

    z

--- protected/views/bspOrder/view.php
+++ protected/views/bspOrder/view.php
@@ -2,41 +2,42 @@
 /* @var $this BspOrderController */
 /* @var $model BspOrder */
 
-$this->breadcrumbs=array(
-	'Bsp Orders'=>array('index'),
-	$model->id,
+$this->breadcrumbs = array(
+    'Bsp Orders' => array('index'),
+    $model->id,
 );
 
-$this->menu=array(
-	array('label'=>'List BspOrder', 'url'=>array('index')),
-	array('label'=>'Create BspOrder', 'url'=>array('create')),
-	array('label'=>'Update BspOrder', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete BspOrder', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage BspOrder', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'List BspOrder', 'url' => array('index')),
+    array('label' => 'Create BspOrder', 'url' => array('create')),
+    array('label' => 'Update BspOrder', 'url' => array('update', 'id' => $model->id)),
+    array('label' => 'Delete BspOrder', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+    array('label' => 'Manage BspOrder', 'url' => array('admin')),
 );
 ?>
 
-<h1>View BspOrder #<?php echo $model->id; ?></h1>
+<h1>View Bsp Order #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'item_id',
-		'buyer_id',
-		'seller_id',
-		'date_order',
-		'date_start',
-		'date_finish',
-		'description',
-		'pph',
-		'comission',
-		'amount',
-		'payment',
-		'status',
-		'create_time',
-		'create_user_id',
-		'update_time',
-		'update_user_id',
-	),
-)); ?>
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+         array('name' => 'item_id', 'value' => !empty($model->item)?$model->item->name:"",),
+        'buyer_id',
+        'seller_id',
+        'date_order',
+        'date_start',
+        'date_finish',
+        'description',
+        'pph',
+        'comission',
+        'amount',
+        'payment',
+        array('name' => 'status', 'value' => $model->status == 1 ? "Enabled" : "Disabled",),
+        'create_time',
+        'create_user_id',
+        'update_time',
+        'update_user_id',
+    ),
+));
+?>
