165cc7a82df2b3b1afc10d865cb6334ef98c584e
ali <itsgeniusstar@gmail.com>
2013-12-27 17:58:54 +0500

    abc def

--- themes/resp_frontend/views/default/index.php
+++ themes/resp_frontend/views/default/index.php
@@ -1,11 +1,65 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-    <h1>Hello, world!</h1>
-    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-    <p><a href="#" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/" . Yii::t('images', 'images/banners/en/1.jpg'));
+            ?>
+            <div class="carousel-caption">
+            </div>
+        </div>
+        <div class="item">
+
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/" . Yii::t('images', 'images/banners/en/1.jpg'));
+            ?>
+            <div class="carousel-caption">
+            </div>
+        </div>
+        <div class="item">
+
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/" . Yii::t('images', 'images/banners/en/2.jpg'));
+            ?>
+            <div class="carousel-caption">
+            </div>
+        </div>
+        <div class="item">
+
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/" . Yii::t('images', 'images/banners/en/3.jpg'));
+            ?>
+            <div class="carousel-caption">
+            </div>
+        </div>
+        <div class="item">
+
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/" . Yii::t('images', 'images/banners/en/4.jpg'));
+            ?>
+            <div class="carousel-caption">
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
 </div>
-
-
 
 <div class="page-header">
     <h1>Buttons</h1>
