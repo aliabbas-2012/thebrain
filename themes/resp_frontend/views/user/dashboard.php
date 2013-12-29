57c921cf6401aa9f7a4674c83db30a2934021e1b
ali <itsgeniusstar@gmail.com>
2013-12-29 20:40:12 +0500

    abc def

--- themes/resp_frontend/views/user/dashboard.php
+++ themes/resp_frontend/views/user/dashboard.php
@@ -8,5 +8,20 @@
     ?>
     <!-- Tab panes -->
 
+    <div class="clear" style="height: 20px;"></div>
+    <label class="hellotext"><?php echo Yii::t('user', 'Hello again') ?> <?php echo Yii::app()->user->user->username; ?>!</label>
+    <div class="clear" style="height: 20px;"></div>
+   
+    <div class="noteleft">
+        <a href="<?php echo $this->createUrl('/web/user/profile'); ?>">
+            <?php echo Yii::t('user', 'Complete your profile') ?>
+        </a> 
+        <?php echo Yii::t('user', 'and atract more interest') ?>
+        <a class="del_box" href="javascript:void(0)" onclick="jQuery(this).parent().remove();">x</a>
+    </div>
+    <div class="noteleft"><a href="<?php echo $this->createUrl('/web/offer/'); ?>" class="post"><?php echo Yii::t('user', 'Post your offer') ?></a></div>
+
+
+
 </div>
 
