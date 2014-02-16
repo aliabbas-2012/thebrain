5b7b74c0aef03150538a6ec8f4474d6f963ac5b3
ali <itsgeniusstar@gmail.com>
2014-02-16 23:19:09 +0500

    abc

--- themes/resp_frontend/views/user/profileview.php
+++ themes/resp_frontend/views/user/profileview.php
@@ -1,35 +1,35 @@
 <h2>
     Profile 
     <?php
-    echo CHtml::link(CHtml::button(Yii::t('user', "Edit Profile"),
-            array("class" => "btn btn-default profile-btn")), $this->createUrl("/web/user/profile"), 
-            array("style" => "float:right"));
+    echo CHtml::link(CHtml::button(Yii::t('user', "Edit Profile"), array("class" => "btn btn-default profile-btn")), $this->createUrl("/web/user/profile"), array("style" => "float:right"));
     ?>
 </h2>
-<?php
-$this->widget('zii.widgets.CDetailView', array(
-    'data' => $model,
-    'attributes' => array(
-        'username',
-        'user_email',
-        'first_name',
-        'second_name',
-        array('name' => 'gender', 'value' => $model->gender == 1 ? "Male" : "Female"),
-        array('name' => 'birthday', 'value' => $model->birthday),
-        array('name' => 'avatar', 'value' => zHtml::imageDoubleLink($model, "avatar", "Users","avatar"),"type"=>"raw"),
-        'paypal_mail',
-        'fbmail',
-        array('name' => 'background', 'value' => zHtml::imageDoubleLink($model, "background", "Users","background"),"type"=>"raw"),
-        'address',
-        'country',
-        'city',
-        'zipcode',
-        'phone',
-        'lat',
-        'lng',
-        array('name' => 'store_url', 'value' => Yii::app()->request->hostInfo . "/" . Yii::app()->baseUrl . "/" . $model->store_url),
-        'description',
-    ),
-));
-?>
+    <?php
+    $store_url = str_replace(" ", "-", $model->store_url);
+    $store_url = $this->createUrl("/web/userdata/store", array("id" => $model->id, "storeurl" => $store_url));
+    $this->widget('zii.widgets.CDetailView', array(
+        'data' => $model,
+        'attributes' => array(
+            'username',
+            'user_email',
+            'first_name',
+            'second_name',
+            array('name' => 'gender', 'value' => $model->gender == 1 ? "Male" : "Female"),
+            array('name' => 'birthday', 'value' => $model->birthday),
+            array('name' => 'avatar', 'value' => zHtml::imageDoubleLink($model, "avatar", "Users", "avatar"), "type" => "raw"),
+            'paypal_mail',
+            'fbmail',
+            array('name' => 'background', 'value' => zHtml::imageDoubleLink($model, "background", "Users", "background"), "type" => "raw"),
+            'address',
+            'country',
+            'city',
+            'zipcode',
+            'phone',
+            'lat',
+            'lng',
+            array('name' => 'store_url', 'value' => $store_url),
+            'description',
+        ),
+    ));
+    ?>
 
