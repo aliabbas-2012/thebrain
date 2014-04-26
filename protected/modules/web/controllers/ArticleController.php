4af18dd987307106c0c82e5413fe6d2772956bcb
ali <ali@192.168.1.3>
2014-04-26 15:12:21 +0500

    paypall email adaptive FROM admin transfer

--- protected/modules/web/controllers/ArticleController.php
+++ protected/modules/web/controllers/ArticleController.php
@@ -6,17 +6,22 @@
  */
 
 class ArticleController extends Controller {
-    
+
     /**
      * 
      * @param type $slug
      */
     public function actionIndex($slug) {
         $slug_arr = explode("-", $slug);
-        
-        $model = BspArticla::model()->findByPk($slug_arr[count($slug_arr) - 1]);
-        
-        $this->render("//article/index",array("model"=>$model));
+        $criteria = new CDbCriteria();
+
+        $criteria->condition = "iStatus = 1";
+        $model = BspArticla::model()->findByPk($slug_arr[count($slug_arr) - 1], $criteria);
+
+        if ($model === null)
+            throw new CHttpException(404, 'Content Not available. We are updating ');
+
+        $this->render("//article/index", array("model" => $model));
     }
 
 }
