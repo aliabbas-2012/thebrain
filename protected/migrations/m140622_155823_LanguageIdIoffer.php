5f4690013b506ae748f77b346d5a1c1fc460f01a
ali <ali@192.168.1.3>
2014-06-22 21:24:02 +0500

    updating language id

--- protected/migrations/m140622_155823_LanguageIdIoffer.php
+++ protected/migrations/m140622_155823_LanguageIdIoffer.php
@@ -5,12 +5,23 @@ class m140622_155823_LanguageIdIoffer extends DTDbMigration {
     public function up() {
         $table = "bsp_item";
         $this->addColumn($table, "language_id", "enum('de','en') after admin_status");
-        
+
+        $sql = "UPDATE bsp_item SET language_id = 'de' WHERE id <=660";
+        $this->execute($sql);
+
+        $sql = "UPDATE bsp_item SET language_id = 'en' WHERE id >660";
+        $this->execute($sql);
     }
 
     public function down() {
         $table = "bsp_item";
+        
+         
+        $sql = "UPDATE bsp_item SET language_id = 'i'";
+        $this->execute($sql);
         $this->dropColumn($table, "language_id");
+
+        
     }
 
 }
