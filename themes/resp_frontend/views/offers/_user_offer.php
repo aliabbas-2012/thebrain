8c7e1f42d193d9266bfa1e0b9756bbfa6b38cb81
ali <ali@192.168.1.3>
2014-07-21 23:24:35 +0500

    map fixing

--- themes/resp_frontend/views/offers/_user_offer.php
+++ themes/resp_frontend/views/offers/_user_offer.php
@@ -67,7 +67,6 @@
                 <?php
                 echo $form->textField($model, 'zipcode', array(
                     'class' => 'k-textbox floatLeft profileBottom profileText',
-                   
                     'placeholder' => Yii::t('postOffer', "Zip Code")));
                 ?> 
 
@@ -98,108 +97,39 @@
     function setCitySearch() {
         //city button
 
-        var markers = [];
-        var map = new google.maps.Map(document.getElementById('map-canvas'), {
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+        var input = document.getElementById('ChangeUser_city');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+
+            document.getElementById('ChangeUser_city').value = place.name;
+            jQuery("#ChangeUser_lat").val(place.geometry.location.lat());
+            jQuery("#BspItemFrontEnd_lat").val(place.geometry.location.lat());
+            jQuery("#ChangeUser_lng").val(place.geometry.location.lng());
+            jQuery("#BspItemFrontEnd_lng").val(place.geometry.location.lng());
+
+
         });
-        //                // Create the search box and link it to the UI element.
-        var input = /** @type {HTMLInputElement} */(
-                document.getElementById('ChangeUser_city'));
-       
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        var searchBox_city = new google.maps.places.SearchBox(
-                /** @type {HTMLInputElement} */(input),{types: ['(cities)'],});
 
 
-
-
-        // [START region_getplaces]
-        // Listen for the event fired when the user selects an item from the
-        // pick list. Retrieve the matching places for that item.
-        google.maps.event.addListener(searchBox_city, 'places_changed', function() {
-            var places = searchBox_city.getPlaces();
-
-            if (typeof(places[0].geometry.location.nb) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.nb);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.nb);
-            }
-            if (typeof(places[0].geometry.location.ob) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.ob);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.ob);
-            }
-
-            //settting changed
-            if (typeof(places[0].geometry.location.A) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.A);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.A);
-            }
-            if (typeof(places[0].geometry.location.k) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.k);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.k);
-            }
-
-            if (typeof(places[0].geometry.location.d) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.d);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.nb);
-            }
-            if (typeof(places[0].geometry.location.e) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.e);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.e);
-            }
-        });
     }
     function setZipSearch() {
         //city button
 
-        var markers = [];
-        var map = new google.maps.Map(document.getElementById('map-canvas'), {
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+        var input = document.getElementById('ChangeUser_zipcode');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+
+            document.getElementById('ChangeUser_zipcode').value = place.name;
+            jQuery("#ChangeUser_lat").val(place.geometry.location.lat());
+            jQuery("#BspItemFrontEnd_lat").val(place.geometry.location.lat());
+            jQuery("#ChangeUser_lng").val(place.geometry.location.lng());
+            jQuery("#BspItemFrontEnd_lng").val(place.geometry.location.lng());
+
+
         });
-        //                // Create the search box and link it to the UI element.
-        var input = /** @type {HTMLInputElement} */(
-                document.getElementById('ChangeUser_zipcode'));
 
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        var searchBox_city = new google.maps.places.SearchBox(
-                /** @type {HTMLInputElement} */(input),{types: ['(zipcodes)'],});
-
-
-
-
-        // [START region_getplaces]
-        // Listen for the event fired when the user selects an item from the
-        // pick list. Retrieve the matching places for that item.
-        google.maps.event.addListener(searchBox_city, 'places_changed', function() {
-            var places = searchBox_city.getPlaces();
-
-            if (typeof(places[0].geometry.location.nb) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.nb);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.nb);
-            }
-            if (typeof(places[0].geometry.location.ob) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.ob);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.ob);
-            }
-
-            //settting changed
-            if (typeof(places[0].geometry.location.A) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.A);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.A);
-            }
-            if (typeof(places[0].geometry.location.k) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.k);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.k);
-            }
-
-            if (typeof(places[0].geometry.location.d) != "undefined") {
-                jQuery("#ChangeUser_lat").val(places[0].geometry.location.d);
-                jQuery("#BspItemFrontEnd_lat").val(places[0].geometry.location.d);
-            }
-            if (typeof(places[0].geometry.location.e) != "undefined") {
-                jQuery("#ChangeUser_lng").val(places[0].geometry.location.e);
-                jQuery("#BspItemFrontEnd_lng").val(places[0].geometry.location.e);
-            }
-        });
     }
-    
+
 </script>
